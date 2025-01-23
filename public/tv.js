76606f36b1b757eb9c044d05504f4cca10415bf5
Maahi Batra <maahi.batra@gmail.com>
2025-01-23 16:17:24 +0530

    trying to fix an error

--- public/tv.js
+++ public/tv.js
@@ -21,6 +21,7 @@ const video = document.createElement("video");
 video.src = "ctv1.mp4";
 video.loop = true;
 video.muted = true;
+video.playsInline = true;
 video.play();
 
 const powerClick = new Audio("powerClick.mp3");
